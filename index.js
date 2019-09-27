2572b6a3452537173e3a5b844e8825b26b412730
Flavien DELANGLE <flaviendelangle@gmail.com>
2019-09-27 16:06:33 +0200

    Add utils

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ module.exports = {
             "/^@query/",
             "/^@routes/",
             "/^@style/",
+            "/^@utils/",
           ],
           "parent",
           ["sibling", "index"]
