a5f926408dedddf504115a6d1839dccbdca7877d
Flavien DELANGLE <flaviendelangle@gmail.com>
2019-09-27 14:18:13 +0200

    Add @globalTypes

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ module.exports = {
             "/^@config/",
             "/^@constants/",
             "/^@fonts/",
+            "/^@globalTypes/",
             "/^@helpers/",
             "/^@hooks/",
             "/^@lib/",
